3552eec6564c0c9eef050458f2de315fb1ea6a1d
Ajay A <ajaycalicut17@gmail.com>
2026-08-01 17:07:09 +0530

    html template

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 )
 
 func main() {
+	tmpl := template.Must(template.ParseGlob("templates/**/*.html"))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "views/login/index.html")
+
+		err := tmpl.ExecuteTemplate(w, "login/index", nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
 
 	http.ListenAndServe(":8080", nil)
